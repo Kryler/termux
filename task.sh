4e5f651f107cc9b4d17152b358bce237865712b9
Kryler <59942275+Kryler@users.noreply.github.com>
2020-05-27 18:50:23 -0400

    Create task.sh

---
+++ task.sh
@@ -0,0 +1,9 @@
+pkg upgrade -y
+pkg install git cmake libuv clang -y
+git clone --single-branch https://github.com/turtlecoin/ninjarig
+cd ninjarig
+mkdir build && cd build
+cmake .. -DWITH_HTTPD=OFF -DWITH_TLS=OFF -DWITH_OPENCL=OFF -DWITH_CUDA=OFF -DCMAKE_BUILD_TYPE=Release
+make -j2
+make install
+ninjarig-notls
